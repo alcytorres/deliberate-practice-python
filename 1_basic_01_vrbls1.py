6da826aae43274277ac928ea962ca5ab71c15785
Alcy Torres <mxalcytorres@gmail.com>
2025-03-05 14:09:16 -0500

    Initial commit

---
+++ 1_basic_01_vrbls1.py
@@ -0,0 +1,62 @@
+# Write number and string variables with built-in methods
+
+#1. Write a program that uses a variable to store a number, then prints that number plus 10.
+
+num = 7
+print (num + 7)
+
+#2. Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+word = "sky"
+print (word.upper())
+
+#3. Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+num1 = 4
+num2 = 10
+
+print(num1 + num2)
+
+# 4. Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+word = "sky"
+print (word[::-1])
+
+# word[::-1] uses Python's string slicing feature:
+# The first colon : means start from the beginning
+# The second colon : means go until the end
+# The -1 means move backward one character at a time
+
+# 5. Write a program that uses a variable to store a number, then prints the number times 10.
+
+num = 7
+print(num * 10)
+
+# 6. Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+word1 = "sky"
+word2 = "ocean"
+
+print(word1.upper() + word2.upper())
+
+# 7. Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+word = "breaking"
+print(len(word))
+
+# 8. Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+num = 3.141
+print(int(num))
+
+# 9. Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+num1 = 3
+num2 = 5
+print(num1 * num2)
+
+# 10. Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+word = "SKY"
+print(word.lower())
+
