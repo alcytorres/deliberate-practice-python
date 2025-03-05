a21073e54b41e8e1ad98c67aeaf9a4462cd65692
Alcy Torres <mxalcytorres@gmail.com>
2025-03-05 16:54:39 -0500

    Update solutions

---
+++ 4_basic_04_arrys_hshs1.py
@@ -0,0 +1,82 @@
+# Write an array, read items from an array, and add new items to existing array
+
+
+# 1. Create an array to store 3 words. Then add two more words to the array and print the array on one line.
+
+words = ["sky", "run", "star"]
+words.append("fly")
+words.append("bat")
+# print(words)
+
+
+# 2. Create an array to store 4 letters. Then change the second letter to a number and print the array on one line.
+
+letters = ["a", "b", "c", "d"]
+letters[1] = 30
+# print(letters)
+
+# 3. Create an array to store 5 numbers. Then print out each number on separate lines with a while loop.
+
+nums = [1, 2, 3, 4, 5]
+i = 0
+
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+
+# 4. Create an array to store 1 number. Then add three more numbers to the array and print the array on one line.
+
+num = [1]
+num.append(2)
+num.append(3)
+num.append(4)
+# print(num)
+
+
+# 5. Create an array to store 3 strings with lower case letters. Then change the third string to have all capital letters and print the array on one line.
+
+strings = ["hello", "hello", "hello"]
+strings[2] = strings[2].upper()
+# print(strings)
+
+
+# 6. Create an array to store 3 names. Then print out each name on separate lines with a while loop.
+
+names = ["John", "Tom", "Mike"]
+i = 0
+
+# while i < len(names):
+#     print(names[i])
+#     i += 1
+
+
+# 7. Create an array to store 2 strings. Then add one string to the array and print the array on one line.
+
+strings = ["Sky", "fly"]
+strings.append("run")
+# print(strings)
+
+
+# 8. Create an array to store 5 numbers. Then change the first number to 10 times its original value and print the array on one line.
+
+nums = [1, 2, 3, 4, 5]
+nums[0] = nums[0] * 10
+# print(nums)
+
+
+# 9. Create an array to store 2 numbers. Then print out each number on separate lines with a while loop.
+
+nums = [1, 3]
+i = 0
+
+# while i < len(nums):
+#     print(nums[i])
+#     i += 1
+
+
+# 10. Create an array to store names of 3 different countries. Then add one more country and print the array one line.
+
+countries = ["Argentina", "Peru", "Ecuador"]
+countries.append("Chile")
+# print(countries)
