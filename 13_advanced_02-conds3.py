260ccd8277978010f1431aaf76085e184fe2a52a
Alcy Torres <mxalcytorres@gmail.com>
2025-03-06 18:26:37 -0500

    Update solutions

--- 13_advanced_02-conds3.py
+++ 13_advanced_02-conds3.py
@@ -7,11 +7,21 @@
 # If the age is between 13 and 59 years old and the movie is before 6 PM, the ticket price is $7. After 6 PM, the ticket price is $10.
 # If the customer is 60 years old or older, the ticket price is $7.
 
+age = 70
+time = 10
 
-age = 17
-time = 23
+if age <= 12:
+    price = 5
+elif age >= 13 and age <= 59:
+    if time < 18:
+        price = 7
+    else:
+        price = 10
+elif age >= 60:
+    price = 7
 
-
+# print(f"Ticket price: ${price}")
+    
 
 # 2. Write a program to store the type of book (regular, reference, or special collection) and the number of days its overdue. Then calculate the fine amount based on the following conditions:
 
@@ -20,11 +30,20 @@ time = 23
 # If the book is a reference book, there is no fine, regardless of the number of days overdue.
 # If the book is a special collection book, the fine is $5 per day, regardless of the number of days overdue.
 
-
 book_type = "special collection"
 days_overdue = 10
-fine_amount = 0
 
+if book_type == "regular":
+    if days_overdue <= 7:
+        fine_amount = 1 * days_overdue
+    elif days_overdue > 7:
+        fine_amount = 2 * days_overdue
+elif book_type == "reference":
+    fine_amount = 0
+elif book_type == "special collection":
+    fine_amount = 5 * days_overdue
+
+# print(f"Fine amount ${fine_amount}")
 
 
 # 3. Write a program that stores a person's order value and membership level (regular or premium). Then calculate a discount amount based on the following conditions:
@@ -33,17 +52,23 @@ fine_amount = 0
 # If the total order value is between $50 and $100, the discount is 5% for regular customers and 10% for premium customers.
 # If the total order value is greater than $100, the discount is 10% for regular customers and 15% for premium customers.
 
-
 order_value = 1000
-membership = "regular"
+membership = "premium"
 
+if order_value < 50:
+    discount = 0
+elif order_value >= 50 and order_value <= 100:
+    if membership == "regular":
+        discount = 0.05
+    elif membership == "premium":
+        discount = 0.1
+elif order_value > 100:
+    if membership == "regular":
+        discount = 0.1
+    elif membership == "premium":
+        discount = 0.15
 
-
-
-# Mistake Explanation: Forgot to add "end".
-  # One time I forgot to add the first end "end".
-  # My mistake was nesting the elsif order_value > 100 inside the elsif order_value > 50 && order_value < 100 block, causing it to only check values between 50 and 100, and skip anything over 100 (like 1000).
-  # It should be a separate, top-level condition.
+# print(f"Discount: ${order_value * discount}")
 
 
 # 4. Write a Ruby program that stores the weight of a package and the destination (domestic or international). Then calculate the shipping fee based on the following conditions:
@@ -55,9 +80,18 @@ membership = "regular"
 # If the weight is less than or equal to 1 kg, the shipping fee is $15.
 # If the weight is greater than 1 kg, the shipping fee is $25.
 
+destination = "international"
+weight = 30
 
-destination = "domestic"
-weight = 4
-shipping_fee = 0
+if destination == "domestic":
+    if weight <= 1:
+        shipping_fee = 5
+    else:
+        shipping_fee = 10
+elif destination == "international":
+    if weight <= 1:
+        shipping_fee = 15
+    else:
+        shipping_fee = 25
 
-
+# print(f"Shipping Fee: ${shipping_fee}")
